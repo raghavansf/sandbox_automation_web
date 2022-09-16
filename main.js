d42fc6129e7a79b534eb04fee2e411e211fa3069
raghavansf <100221094+raghavansf@users.noreply.github.com>
2022-09-16 12:20:32 +0100

    Included logger imports

--- main.js
+++ main.js
@@ -1,18 +1,25 @@
-import express from "express";
-import http from "http";
-import {} from "dotenv/config";
+import express from 'express';
+import http from 'http';
+import {} from 'dotenv/config';
+import logger from './logger.js';
 const app = express();
-app.set("port", process.env.PORT || 5000);
+app.set('port', process.env.PORT || 5000);
 
 // Try to define BasePath and routes ..
 
 // Endpoints
 
-app.use("/config", function (req, res) {
-  logger.log('info', 'Displaying ENV values ..', { message : process.env.DB_HOST });
+app.use('/config', function (req, res) {
+  logger.log('info', 'Displaying ENV values ..', {
+    message: process.env.DB_HOST,
+  });
 });
 
 const server = http.createServer(app);
 server.listen(app.get('port'), () =>
-  logger.info(`Provisioning Manager App listening to port ${app.get('port')} for accepting requests`)
+  logger.info(
+    `Provisioning Manager App listening to port ${app.get(
+      'port'
+    )} for accepting requests`
+  )
 );
